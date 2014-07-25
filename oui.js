bb4cf2a89c02d139973dd05d1ecf258f4b7371f3
silverwind <me@silverwind.io>
2014-07-25 16:56:55 +0200

    Print version on the CLI

--- oui.js
+++ oui.js
@@ -107,14 +107,17 @@ if (!module.parent && process.argv.length >= 2) {
             }
         });
     } else if (!arg) {
-        process.stdout.write([
-            "Usage: ",
-            "",
-            "  oui MAC                 Look up an MAC-address or OUI prefix.",
-            "  oui --update            Update the database.\n"
-
-        ].join("\n"));
-        process.exit(1);
+        fs.readFile(path.join(__dirname + "/package.json"), function (err, data) {
+            var pkg = JSON.parse(data);
+            process.stdout.write([
+                pkg.name + "@" + pkg.version + " - " + pkg.homepage,
+                "",
+                "Usage: ",
+                "  oui MAC                 Look up an MAC-address or OUI prefix.",
+                "  oui --update            Update the database.\n"
+            ].join("\n"));
+            process.exit(1);
+        });
     } else {
         oui(arg, function (err, result) {
             if (err) {
