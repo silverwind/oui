e379889f8af4d4f8e3ae66356da10ba48525e878
silverwind <me@silverwind.io>
2014-08-09 20:41:44 +0200

    Add Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+"use strict";
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        bump: {
+            options: {
+                files: ["package.json"],
+                commit: true,
+                commitMessage: "Release %VERSION%",
+                commitFiles: ["package.json"],
+                createTag: true,
+                tagName: "%VERSION%",
+                tagMessage: "Version %VERSION%",
+                push: false
+            }
+        },
+        shell: {
+            options: {
+                stdout: true,
+                stderr: true,
+                failOnError: true
+            },
+            push: {
+                command: "git push -u -f --tags origin master"
+            },
+            publish: {
+                command: "npm publish"
+            },
+            update: {
+                command: "npm-check-updates -u"
+            },
+            modules: {
+                command: "rm -rf node_modules && npm install"
+            }
+        }
+    });
+
+    grunt.registerTask("update",  ["shell:update", "shell:modules"]);
+    grunt.registerTask("patch",   ["bump", "shell:push", "shell:publish"]);
+    grunt.registerTask("release", ["bump:minor", "shell:push", "shell:publish"]);
+
+    grunt.loadNpmTasks("grunt-bump");
+    grunt.loadNpmTasks("grunt-shell");
+};
