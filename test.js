e2e86b7a7ed3ca87ec7ed21a45e39953456d91c8
silverwind <me@silverwind.io>
2017-01-20 16:57:58 +0100

    add tests and .travis.yml

---
+++ test.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const assert = require("assert");
+const oui = require(".");
+
+assert.ok(/cisco/i.test(oui("203706")));
+assert.ok(/cisco/i.test(oui("20:37:06")));
+assert.ok(/cisco/i.test(oui("20:37:06:11:22:33")));
+
+assert.equal(oui("20370"), null);
+
+assert.throws(() => { oui(null); }, Error);
+assert.throws(() => { oui(undefined); }, Error);
+assert.throws(() => { oui([]); }, Error);
+assert.throws(() => { oui("20370", {strict: true}); }, Error);
+assert.throws(() => { oui("abcd", {strict: true}); }, Error);
+assert.throws(() => { oui(null, {strict: true}); }, Error);
+assert.throws(() => { oui(undefined, {strict: true}); }, Error);
