96a80b79463c698a4219b631bdbabffa509dc825
silverwind <me@silverwind.io>
2018-10-13 11:06:25 +0200

    9.0.7

--- oui.web.js
+++ oui.web.js
@@ -1,4 +1,4 @@
-/*! oui web v9.0.6 | (c) silverwind | BSD license */
+/*! oui web v9.0.7 | (c) silverwind | BSD license */
 /* eslint-disable no-var */
 (function(m) {
   if (typeof exports === "object" && typeof module === "object") {
