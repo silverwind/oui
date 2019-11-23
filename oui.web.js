d8c60f161bd1f8beeced0d635dfbffab2a920776
silverwind <me@silverwind.io>
2019-11-23 11:06:33 +0100

    10.0.32
    * automated weekly data update (silverwind)
    * update dependencies (silverwind)
    * switch to 'versions' (silverwind)

--- oui.web.js
+++ oui.web.js
@@ -1,4 +1,4 @@
-/*! oui web v10.0.31 | (c) silverwind | BSD license */
+/*! oui web v10.0.32 | (c) silverwind | BSD license */
 /* eslint-disable no-var */
 (function(m) {
   if (typeof exports === "object" && typeof module === "object") {
