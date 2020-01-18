7674cae82876352fc7c022c64ad99dee6a234a9c
silverwind <me@silverwind.io>
2020-01-18 11:06:44 +0100

    10.0.40
    * automated weekly data update (silverwind)

--- oui.web.js
+++ oui.web.js
@@ -1,4 +1,4 @@
-/*! oui web v10.0.39 | (c) silverwind | BSD license */
+/*! oui web v10.0.40 | (c) silverwind | BSD license */
 /* eslint-disable no-var */
 (function(m) {
   if (typeof exports === "object" && typeof module === "object") {
