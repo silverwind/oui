a06266b88a0790e28ef5a8aac7136f27e31d3096
silverwind <me@silverwind.io>
2023-10-06 01:43:55 +0200

    lint

--- oui.test.js
+++ oui.test.js
@@ -1,7 +1,7 @@
 import {execa} from "execa";
 
 test("cli", async () => {
-  const {stdout, stderr, exitCode} = await execa("node", ["--no-warnings", "./bin/oui.js", "20:37:06:12:34:56"]);
+  const {stdout, exitCode} = await execa("node", ["--no-warnings", "./bin/oui.js", "20:37:06:12:34:56"]);
   expect(stdout).toMatchSnapshot();
   expect(exitCode).toEqual(0);
 });
